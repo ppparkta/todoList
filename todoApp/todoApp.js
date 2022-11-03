0c1a5533a160b1aa680043f45d9c3d6cf8446da9
ppparkta <86940801+ppparkta@users.noreply.github.com>
2022-11-03 20:29:45 +0900

    221103 add Login

---
+++ todoApp/todoApp.js
@@ -0,0 +1,26 @@
+const $loginForm = document.querySelector("#login-form");
+const $logintInput = document.querySelector("#login-form input");
+const $greeting = document.querySelector("#greeting");
+const USERNAME_KEY="username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function paintGreeting(username){
+	$greeting.textContent = `Hello ${username}`;
+	$greeting.classList.remove("hidden");
+}
+
+function onLoginSubmit(event) {
+	event.preventDefault();
+	const userName = $logintInput.value;
+	$loginForm.classList.add("hidden");
+	paintGreeting(userName);
+	localStorage.setItem(USERNAME_KEY, userName);
+}
+
+if (!savedUsername) {
+	$loginForm.classList.remove("hidden");
+	$loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+	paintGreeting(savedUsername);
+}
