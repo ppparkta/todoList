61baa0edcf3155782f0129e7e57fc8f6fa98cd05
양수빈 <soobin@yangsubin-ui-MacBookAir.local>
2022-11-06 15:17:09 +0900

    221106 di

--- index.js
+++
@@ -1,7 +0,0 @@
-const $hello=document.querySelector(".hello");
-const $h1=document.querySelector("div.hello h1");
-
-function handleTitleClick(){
-	$h1.classList.toggle("active")
-};
-$h1.addEventListener("click",handleTitleClick);
